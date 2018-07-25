d811060514eab0e6e359db8ef2d9728ce16cd48e
unknown <agabrieladuran1@gmail.com>
2018-07-25 15:00:34 -0400

    validaciones HTTP y creacion de carpeta views question

--- api/controllers/question.js
+++ api/controllers/question.js
@@ -1,12 +1,10 @@
 import question from "../models/question"
 import mongoose from "mongoose"
 
-
+//CREAR UNA NUEVA PREGUNTA
 function newQuestion(req, res) {
-    console.log( "aqui en la funcion new question ")
-    
- 
- const pregunta = new question({
+
+    const pregunta = new question({
         
         _id: new mongoose.Types.ObjectId(),
         question: req.body.question,
@@ -19,72 +17,82 @@ function newQuestion(req, res) {
    
    console.log(pregunta)
    res.json(pregunta)
-
-    pregunta.save()
-            .then(nuevapregunta => {
-             res.status(201).json(nuevapregunta)
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({error: err})
-            }); 
+    try {
+        pregunta.save()
+                .then(nuevapregunta => {
+                     res.status(201).json(nuevapregunta)
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({error: err})
+                }); 
+    }
+    catch(error){
+        console.log(error)
+    }
 
 }
 
+
+//BUSCAR TODAS LAS PREGUNTAS DE LA BD
 function getQuestions(req, res)
 {
+  try{
     question.find()
-    .exec()
-    .then(questions => { 
-        if(questions) {
-          res.status(200).json(questions)
-        }else{
-            res.status(404).json({message: 'no hay preguntas'})
-        }   
-        
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({error: err})
-    })
-}
-
-function getQuestionID(req, res){
-    const id = req.params.questionID;
-   
-    if(mongoose.Types.ObjectId.isValid(id))
-    {
-    question.findById(id)
             .exec()
-            .then(questionByID =>{
-                
-                if(questionByID){
-                    res.status(200).json(questionByID)
-                }
-                else {
-                    res.status(404).json({message: 'no encontrado, ID incorrecto'})
-                }
-                
-            })
-            .catch(err=>{
+            .then(questions => { 
+                    if(questions) {
+                        res.status(200).json(questions)
+                    }else{
+                        res.status(404).json({message: 'no hay preguntas'})
+                    }   
+        
+            }).catch(err => {
                 console.log(err);
                 res.status(500).json({error: err})
             })
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+//BUSCAR UNA PREGUNTA POR SU ID
+function getQuestionID(req, res){
+
+    const id = req.params.questionID;
+   
+    if(mongoose.Types.ObjectId.isValid(id)){
+        question.findById(id)
+                .exec()
+                .then(questionByID =>{                
+                    if(questionByID){
+                    res.status(200).json(questionByID)
+                    }
+                    else {
+                    res.status(404).json({message: 'no encontrado, ID incorrecto'})
+                    }                
+                })
+                .catch(err=>{
+                    console.log(err);
+                    res.status(500).json({error: err})
+                })
     }
     else{
         res.status(404).json({message: "no valid entry for provided ID"})
     }
 }
 
+
+//ELIMINAR UNA PREGUNTA
 function deleteQuestionByID(req,res){
-    const id = req.params.questionID;
+
+    const id = req.params.questionID;    
     
-    
-    if(mongoose.Types.ObjectId.isValid(id))
-    {
+    if(mongoose.Types.ObjectId.isValid(id)){
         question.findByIdAndRemove(id).exec().then(result=>{
             if(result){
-            res.status(200).json(result)
-         
+                res.status(200).json(result)         
             }
             else {
                 res.status(404).json({message: "ERROR ID"})
@@ -101,21 +109,18 @@ function deleteQuestionByID(req,res){
 }
 
 
+//MODIFICAR DATA DE UNA PREGUNTA 
 function updateQuestionByID(req, res){
     //const id = mongoose.Types.ObjectId(req.body.id)
     const id = req.params.questionID;
-   console.log(id);
    
- 
     if (mongoose.Types.ObjectId.isValid(id)) {
         question.where({'_id': id})
                 .update( {$set: {question: req.body.question, options: req.body.options, answer: req.body.answer, level: req.body.level, category: req.body.category}})
                 .exec()
-                .then(result =>{
-                    
+                .then(result =>{                    
                     if(result.nModified===1){
-                    res.status(200).json(question)
-                       
+                    res.status(200).json(question)                       
                     }
                     else {                        
                         res.status(404).json({message: 'no encontrado'})
@@ -127,7 +132,7 @@ function updateQuestionByID(req, res){
     }
     else{
         res.status(404).json({message: 'error de id, incorrecto'})
-       }
+    }
  
 }
 module.exports= {newQuestion, getQuestions, getQuestionID, deleteQuestionByID, updateQuestionByID}
