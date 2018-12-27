e1de60755632e77d367e9df2e0bad043906f930b
Unknown <agabrieladuran1@gmail.com>
2018-12-27 19:26:16 -0400

    validaciones req.check de variables de configuracion

--- api/controllers/variable.js
+++ api/controllers/variable.js
@@ -2,25 +2,38 @@ import mongoose from "mongoose"
 import variable from "../models/variable"
 
 function newVariable(req,res,next) {
+
+    req.check("title").notEmpty().withMessage("El campo de titulo no puede estar vacio") 
+    req.check("description").notEmpty().withMessage("El campo de descripcion no puede estar vacio")
+    req.check('description').isLength({min: 10}).withMessage('La descripcion es muy corta') 
+    req.check('title').isLength({min: 5}).withMessage('El titulo es muy corto') 
     
-    const variableC = new variable({
-        _id: new mongoose.Types.ObjectId(),
-        title: req.body.title,
-        description: req.body.description
-        });
-       
-        try{ 
-            variableC.save()
-                    .then(nuevavariable => { 
-                        res.status(201).render('variable/variableDetail',{variableC: nuevavariable})
-                     })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(500).json({error: err})
-                    }); 
-        }  
-        catch(error){
-           console.log(error) 
+
+    var errors = req.validationErrors();
+    if (errors){
+        res.render('variable/newVariable', {error: errors});
+        return;
+    } else {
+        const variableC = new variable({
+            _id: new mongoose.Types.ObjectId(),
+            title: req.body.title,
+            description: req.body.description
+            });
+        
+            try{ 
+                variableC.save()
+                        .then(nuevavariable => { 
+                            res.status(201).render('variable/variableDetail',{variableC: nuevavariable})
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({error: err})
+                        }); 
+            }  
+            catch(error){
+            console.log(error);
+            }
+
         }
 }
 
@@ -73,28 +86,39 @@ function getVariableID(req,res,next){
 }
 
 function updateVariableByID(req, res, next){
+   
+    req.check("title").notEmpty().withMessage("El campo de titulo no puede estar vacio") 
+    req.check("description").notEmpty().withMessage("El campo de descripcion no puede estar vacio")
+    req.check('description').isLength({min: 10}).withMessage('La descripcion es muy corta') 
+    req.check('title').isLength({min: 5}).withMessage('El titulo es muy corto') 
+    
+    var errors = req.validationErrors();
+    if (errors){
+        res.render('variable/updateVariable', {error: errors, variableC: req.body});
+        return;
+    } else {
+        const id = mongoose.Types.ObjectId(req.body._id)
+    //const id = req.params.variableID;
 
-    //const id = mongoose.Types.ObjectId(req.body._id)
-    const id = req.params.variableID;
-
-    if (mongoose.Types.ObjectId.isValid(id)) {
-        variable.where({'_id': id})
-                .update( {$set: {title: req.body.title, description: req.body.description}})
-                .exec()
-                .then(result =>{                    
-                    if(result.nModified===1){
-                    res.status(200).render('variable/variableDetail',{variableC: req.body})                      
-                    }
-                    else {                        
-                        res.status(404).json({message: 'no encontrado'})
-                    }
-                })
-                .catch(err =>{
-                    res.status(500).json({error: err})
-                })
-    }
-    else{
-        res.status(404).json({message: 'error de id, incorrecto'})
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            variable.where({'_id': id})
+                    .update( {$set: {title: req.body.title, description: req.body.description}})
+                    .exec()
+                    .then(result =>{                    
+                        if(result.nModified===1){
+                        res.status(200).render('variable/variableDetail',{variableC: req.body})                      
+                        }
+                        else {                        
+                            res.status(404).json({message: 'no encontrado'})
+                        }
+                    })
+                    .catch(err =>{
+                        res.status(500).json({error: err})
+                    })
+        }
+        else{
+            res.status(404).json({message: 'error de id, incorrecto'})
+        }
     }
 }
 
@@ -131,7 +155,7 @@ function editVariable(req,res, next){
                 variable.findById(id)
                 .exec()
                 .then(variableByID =>{           
-                    res.render('variable/updateVariable', {config: variableByID})
+                    res.render('variable/updateVariable', {variableC: variableByID})
                 }).catch(err=> {
                     res.status(404).json({message: "no valid entry for provided ID"})
                 })
