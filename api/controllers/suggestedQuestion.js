d666c9b30384761fe41cc542b7d5879abe8ee90e
unknown <agabrieladuran1@gmail.com>
2019-02-17 20:08:16 -0400

    commit control

--- api/controllers/suggestedQuestion.js
+++ api/controllers/suggestedQuestion.js
@@ -3,106 +3,109 @@ import suggestedQuestion from "../models/suggestedQuestion"
 import question from "../models/question"
 
 
+function aporteFuncional(){
 
-function getSuggestedQuestion(req,res,next){
-  var arrayCaracteres = [];
-var regexEspeciales = /[`~!@#$%^&*()_°¬|+\-=?;:'",.<>\{\}\[\]\\\/]{4}/;
+var regexEspeciales = /[`~!@#$%^&*()_°¬|+\-=?;:'",.<>\{\}\[\]\\\/]{3}/;
 var palabrasConsecutivas = /[a-z]{4}$/;
-  var trivia = [];
-  var allObject = [];
-  var contador= 0;
+var trivia = [];
+var allObject = [];
+var contador= 0;
+    suggestedQuestion.find()
+    .exec()
+    .then(suggested => {      
+        if(suggested)    {  
+           // SI LA PREGUNTA TIENE UNICAMENTE UNA PALABRA
+            for(let i=0; i < suggested.length; i++ ){
+             
+                var separacion = suggested[i].question.split(" ");
+               
+                if((separacion.length==0) || (separacion.length==1)){
+                    suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
+                    
+                }
+                
+            }
+            // SI UNA MISMA PALABRA ESTA EN LAS OPCIONES A B Y C
+            for(let i=1; i < suggested.length; i++ ){
+              trivia.push(suggested[i].question.toLowerCase(), suggested[i].correctAnswer.toLowerCase(), suggested[i].optionB.toLowerCase(), suggested[i].optionC.toLowerCase());
+                var separacion2 = trivia[1].split(" ");
+                var separacion3 = trivia[2].split(" ");
+                var separacion4 = trivia[3].split(" ");
+             
+                
+                for(let i=0; i < separacion2.length; i++ ){
+                    allObject.push(separacion2[i]);
+
+                }
+                    
+                for(let j=0; j < separacion3.length; j++ ){
+                    allObject.push(separacion3[j]);
+                } 
+
+                for(let k=0; k < separacion4.length; k++ ){
+                       allObject.push(separacion4[k]);
+                } 
+                       
+                       
+                for(let m=0; m< allObject.length; m++) {
+                    if(allObject[m]==allObject[m+1]){
+                        contador++;
+                    }
+                   
+                }
+
+                if(contador>0){
+                    suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
+                }
+                contador=0;
+                trivia=[];
+                allObject=[];
+            }
+           
+            //SI LAS OPCIONES TIENE MAS DE 3 CAMPOS
+           for(let i=0; i < suggested.length; i++ ){
+                
+                var separacion6 = suggested[i].correctAnswer.split(" ");
+                var separacion7 = suggested[i].optionB.split(" ");
+                var separacion8 = suggested[i].optionC.split(" ");                          
+               
+                if((separacion6.length > 3) || (separacion7.length > 3) || (separacion8.length > 3)) {
+                    suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
+                    
+                }
+                                                   
+            }
+            // SI EL LENGHT DE LAS OPCIONES PASA DE 30
+            for(let i=0; i < suggested.length; i++ ){                        
+               
+                if((suggested[i].correctAnswer.length > 25) || (suggested[i].optionB.length > 25) || (suggested[i].optionC.length > 25)) {
+                    suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
+                    
+                }
+                                                   
+            }
+
+            //SI TIENE CARACTERES ALFABETICOS CONSECUTIVOS
+            
+            /*for(let i=0; i < suggested.length; i++ ){                        
+               
+                if(palabrasConsecutivas.test(suggested[i].question) }}) {
+                    suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
+                    
+                }
+
+                var
+                                                   
+            }*/
+        }
+    });
+}
+function getSuggestedQuestion(req,res,next){
+    aporteFuncional();
     suggestedQuestion.find()
                         .exec()
                         .then(suggested => {      
-                            if(suggested)    {  
-                               // SI LA PREGUNTA TIENE UNICAMENTE UNA PALABRA
-                                for(let i=0; i < suggested.length; i++ ){
-                                 
-                                    var separacion = suggested[i].question.split(" ");
-                                   
-                                    if((separacion.length==0) || (separacion.length==1)){
-                                        suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
-                                        
-                                    }
-                                    
-                                }
-                                // SI UNA MISMA PALABRA ESTA EN LAS OPCIONES A B Y C
-                                for(let i=1; i < suggested.length; i++ ){
-                                  trivia.push(suggested[i].question.toLowerCase(), suggested[i].correctAnswer.toLowerCase(), suggested[i].optionB.toLowerCase(), suggested[i].optionC.toLowerCase());
-                                    var separacion2 = trivia[1].split(" ");
-                                    var separacion3 = trivia[2].split(" ");
-                                    var separacion4 = trivia[3].split(" ");
-                                 
-                                    
-                                    for(let i=0; i < separacion2.length; i++ ){
-                                        allObject.push(separacion2[i]);
-    
-                                    }
-                                        
-                                    for(let j=0; j < separacion3.length; j++ ){
-                                        allObject.push(separacion3[j]);
-                                    } 
-    
-                                    for(let k=0; k < separacion4.length; k++ ){
-                                           allObject.push(separacion4[k]);
-                                    } 
-                                           
-                                           
-                                    for(let m=0; m< allObject.length; m++) {
-                                        if(allObject[m]==allObject[m+1]){
-                                            contador++;
-                                        }
-                                       
-                                    }
-    
-                                    if(contador>0){
-                                        suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
-                                    }
-                                    contador=0;
-                                    trivia=[];
-                                    allObject=[];
-                                }
-                               
-                                //SI LAS OPCIONES TIENE MAS DE 3 CAMPOS
-                               for(let i=0; i < suggested.length; i++ ){
-                                    
-                                    var separacion6 = suggested[i].correctAnswer.split(" ");
-                                    var separacion7 = suggested[i].optionB.split(" ");
-                                    var separacion8 = suggested[i].optionC.split(" ");                          
-                                   
-                                    if((separacion6.length > 3) || (separacion7.length > 3) || (separacion8.length > 3)) {
-                                        suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
-                                        
-                                    }
-                                                                       
-                                }
-                                // SI EL LENGHT DE LAS OPCIONES PASA DE 30
-                                for(let i=0; i < suggested.length; i++ ){                        
-                                   
-                                    if((suggested[i].correctAnswer.length > 25) || (suggested[i].optionB.length > 25) || (suggested[i].optionC.length > 25)) {
-                                        suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
-                                        
-                                    }
-                                                                       
-                                }
-
-                                //SI TIENE CARACTERES ALFABETICOS CONSECUTIVOS
-                                
-                                /*for(let i=0; i < suggested.length; i++ ){                        
-                                   
-                                    if(palabrasConsecutivas.test(suggested[i].question) }}) {
-                                        suggestedQuestion.findByIdAndRemove(suggested[i]._id).exec();
-                                        
-                                    }
-
-                                    var
-                                                                       
-                                }*/
-                                console.log(suggested[3]);
-                                var prueba = regexEspeciales.test(suggested[3].question);
-                                 
-                                console.log(prueba);
-
+                            if(suggested)    {    
 
                                 res.status(200).render('suggested/suggestedAll',{preguntas: suggested})
                                  
